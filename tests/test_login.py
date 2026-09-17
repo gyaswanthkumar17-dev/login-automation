55b48c5347492c56ae8c79544da2e70ccf8aac4c
gyaswanthkumar17-dev <g.yaswanth.kumar17@gmail.com>
2026-09-17 10:48:31 +0530

    Add headless Chrome options for Jenkins

--- tests/test_login.py
+++ tests/test_login.py
@@ -10,7 +10,10 @@ import time
 @pytest.fixture
 def driver():
     options = Options()
-    options.add_argument("--start-maximized")
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
     yield driver
